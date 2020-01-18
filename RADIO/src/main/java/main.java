84d4de6ebdb471181810c31b73425cb1b7073d07
marisaamv <mon19169@uvg.edu.gt>
2020-01-18 17:26:17 -0600

    Comienzo en Main, creacion de interfaz

--- RADIO/src/main/java/main.java
+++ RADIO/src/main/java/main.java
@@ -4,10 +4,24 @@
  * and open the template in the editor.
  */
 
-/**
- *
- * @author Maria José
- */
+/* Universidad del Valle
+Maria Jose Morales 19145
+Maria Isabel Montoya 19169
+Hoja de Trabajo 1
+Algoritmos y Estructura de Datos
+Clase: Main
+*/
+import.util.Scanner;
 public class main {
+    public static void main(String[]args){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Bienvenido a su radio");
+        System.out.println("Opciones: \n 1. Encender \n 2. Cambiar de AM a FM a AM \n 3. Avanzar en el dial \n 4. Guardar Emisora \n 5. Seleccionar emisora \n 6. Apagar");
+        System.out.println("Ingrese su opcion:");
+        int op = scan.next.Int();
+        if (op==1){
+                
+                
+    }
     
 }
