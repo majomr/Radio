8d2ebabfa95969d1a782c985d185c8be73623ab8
marisaamv <mon19169@uvg.edu.gt>
2020-01-18 17:50:34 -0600

    Variables

--- RADIO/src/main/java/radio.java
+++ RADIO/src/main/java/radio.java
@@ -11,6 +11,21 @@ Hoja de Trabajo 1
 Algoritmos y Estructuras de Datos
 Clase: radio
  */
-public class radio {
+public class radio implements radioInterfaz {
+    // Se definen las variables de la clase
+    private String estado;
+    private String frecuencia;
+    private float emisoraAM;
+    private float emisoraFM;
+    //MAJO no me recuerdo si habiamos dicho que float o string al final amix
+    private float[] AM;
+    private float[] FM;
     
+    public String estacionActual(){
+        return "hola";
+        
+    }
+    public void guardar(){
+        
+    }
 }
